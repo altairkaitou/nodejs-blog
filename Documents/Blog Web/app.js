e63bfcbf453411ea241787aa302ab76564628fe3
Minh Bao <leminhgiabao@gmail.com>
2024-08-28 08:48:52 +0700

    Page Featured

--- Documents/Blog Web/app.js
+++ Documents/Blog Web/app.js
@@ -3,6 +3,8 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 const expressLayout = require('express-ejs-layouts');
+const connectDB = require('./server/config/db');
+
 
 
 app.use(express.static('public'));
@@ -15,6 +17,10 @@ app.set('view engine', 'ejs'); //Set view engine is ejs
 
 
 const PORT = 3000 || process.env.PORT;
+
+
+// Connect to DB
+connectDB();
 
 app.use('/', require('./server/routes/main'));
 
--- Documents/Blog Web/server/routes/main.js
+++ Documents/Blog Web/server/routes/main.js
@@ -1,16 +1,111 @@
 const express = require('express');
 const router = express.Router();
+const Post = require('../models/Post');
+
 
 //Routes
 
-router.get('', (req, res) => {
+
+/**
+ * GET /
+ * HOME
+*/
+
+router.get('', async (req, res) => {
+
+    try {
+        const locals = {
+            title: "Pratice Blog",
+            description: "A simple blog created with Nodejs, Express & MongoDB"
+        }
+
+        let perPage = 1;
+        let page = req.query.page || 1;
+
+        const data = await Post.aggregate([ { $sort: {createdAt: -1} } ])
+        .skip(perPage *  page - perPage)
+        .limit(perPage)
+        .exec();
+
+        const count = await Post.countDocuments();
+
+        const nextPage = parseInt(page) + 1;
+        const hasNextPage = nextPage <= Math.ceil(count / perPage);
+        
+    
+        res.render('index', {
+            locals,
+            data,
+            current: page,
+            nextPage: hasNextPage ? nextPage : null
+        });
+
+    } catch (error) {
+        console.log(error);
+    }
+
+});
+
+
+
+
+// router.get('', async (req, res) => {
+//     const locals = {
+//         title: "Pratice Blog",
+//         description: "A simple blog created with Nodejs, Express & MongoDB"
+//     }
+
+//     try {
+//         const data = await Post.find();
+//         res.render('index', {locals, data});
+
+//     } catch (error) {
+//         console.log(error);
+//     }
+
+// });
+
+
+
+// function insertPostData() {
+//     Post.insertMany([
+//         {
+//             title: "Building a Blog",
+//             body: "This is the body text"
+
+//         },
+//         {
+//             title: "Learn Morgan - HTTP Request logger for NodeJs",
+//             body: "Learn Morgan"
+//         }
+//     ])
+// }
+
+// insertPostData();
+
+
+
+/**
+ * GET /
+ * POST : id
+*/
+
+router.get('/post/:id', async (req, res) => {
     const locals = {
         title: "Pratice Blog",
         description: "A simple blog created with Nodejs, Express & MongoDB"
     }
-    res.render('index', {locals});
 
-})
+    try {
+        const data = await Post.find();
+        res.render('index', {locals, data});
+
+    } catch (error) {
+        console.log(error);
+    }
+
+});
+
 
 router.get('/about', (req, res) => {
     res.render('about');
